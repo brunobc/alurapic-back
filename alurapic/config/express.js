a341d4550de0f1a2240e1d52f9dd178a0b22c649
brunobc <brunobezerrachaves@gmail.com>
2016-02-25 20:08:50 -0300

    Aula 8

--- alurapic/config/express.js
+++ alurapic/config/express.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var consign = require('consign');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static('./public'));
+
+app.use(bodyParser.json());
 
 //require('../app/routes/foto')(app);
 //require('../app/routes/grupo')(app);
